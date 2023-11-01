346c5308cf94b5081787d41dc200a423cadaca8b
YarkoMarko <Yarema1310@gmail.com>
2023-11-01 20:11:10 +0200

    Test how class works

---
+++ main.py
@@ -0,0 +1,26 @@
+class Fraction:
+    numerator = None
+    denominator = None
+
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __add__(self, other):
+        if self.denominator == other.denominator:
+            return Fraction(self.numerator + other.numerator, self.denominator)
+
+        else:
+            new_denominator = self.denominator * other.denominator
+            new_numerator1 = self.numerator * other.denominator
+            new_numerator2 = other.numerator * self.denominator
+
+            return Fraction(new_numerator1 + new_numerator2, new_denominator)
+
+    def show(self):
+        print(f"{self.numerator} / {self.denominator}")
+
+
+fr = Fraction(4, 7)
+
+fr.show()
